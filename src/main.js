0041c5736adca5d6b9edc243cffee3427de2b98f
Benjamin Bock <26689948+MrBock@users.noreply.github.com>
2019-08-03 16:34:51 +0200

    Add array with random timeouts; loop over array and invert background

--- src/main.js
+++ src/main.js
@@ -1,6 +1,37 @@
 const body = document.body;
+
+function toggleBackgroundColor() {
+  body.classList.toggle('is-inverted');
+}
 body.addEventListener('keydown', function(e) {
   if (e.keyCode === 32) {
-    body.classList.toggle('is-inverted');
+    toggleBackgroundColor();
   }
 });
+
+function createTimingArray(numbersInArray) {
+  const array = [];
+  for (let i = 0; i < numbersInArray; i++) {
+    const randomNumber = Math.round(Math.random() * (2000 - 500) + 500);
+    array.push(randomNumber);
+  }
+  return array;
+}
+
+const firstRound = createTimingArray(5);
+console.log(firstRound);
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), ms);
+  });
+}
+
+async function showChangeRhythm() {
+  for (let i of firstRound) {
+    await sleep(i);
+    toggleBackgroundColor();
+  }
+}
+
+showChangeRhythm();
